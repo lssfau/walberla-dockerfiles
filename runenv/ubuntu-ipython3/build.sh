71b3d6f1d5b06a09a2a42530d0f39a63d88f1153
Martin Bauer <martin.bauer@fau.de>
2016-03-13 14:11:05 +0100

    Adaptations for new llvmlite version

--- runenv/ubuntu-ipython3/build.sh
+++ runenv/ubuntu-ipython3/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Clone waLBerla from internal repository ( access required )
-git clone git@i10git.cs.fau.de:software/walberla.git
+#git clone git@i10git.cs.fau.de:software/walberla.git
 # Add the source code to private image and build waLBerla there
 sudo docker build -t walberla/runenv-ubuntu-python-build -f Dockerfile.build .
 # Copy the build results
-sudo docker run walberla/runenv-ubuntu-python-build > pywaLBerla.tar.gz
+sudo docker run --rm walberla/runenv-ubuntu-python-build > pywaLBerla.tar.gz
 # Create distribution container that does not contain sources ...
 sudo docker build -t walberla/runenv-ubuntu-python -f Dockerfile.dist .
 # ... and publish it
