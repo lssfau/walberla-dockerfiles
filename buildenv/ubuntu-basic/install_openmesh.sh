ffd86c7e0766ae4e4290a15cf33e3bf8170c2e64
Christian Godenschwager <christian.godenschwager@fau.de>
2016-12-19 14:46:13 +0100

    Added openmesh to base images

---
+++ buildenv/ubuntu-basic/install_openmesh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+OPENMESH_VERSION=6.3
+TMP="/tmp/openmesh-$OPENMESH_VERSION"
+mkdir $TMP
+cd $TMP
+
+curl -L https://www.openmesh.org/media/Releases/6.3/OpenMesh-6.3.tar.bz2 | tar xj
+cd OpenMesh-$OPENMESH_VERSION
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_APPS=OFF ..
+make -j `nproc`
+make -j `nproc` install
+
+cd && rm -rf $TMP
+
